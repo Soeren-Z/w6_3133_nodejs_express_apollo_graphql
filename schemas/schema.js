9c9efea491f1059325884032f23ccadc3c8be903
Harrison Pape <harrison.pape@georgebrown.ca>
2026-02-20 16:32:20 -0500

    GraphQL schema created

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,41 @@
 import { gql } from 'graphql-tag';
+
+const movieSchema = gql`
+    
+    type Movie {
+        id: String!
+        name: String
+        director_name: String!
+        production_house: String!
+        release_date: String!
+        rating: FlOAT!
+    }
+
+    type Query {
+        getMovies: [Movie]
+        getMovieById(id: String): Movie
+        getMoviesByDirector(director_name: String!): [Movie]
+    }
+
+    type Mutation {
+        addMovie(
+            name: String
+            director_name: String!
+            production_house: String!
+            release_date: String!
+            rating: FlOAT!
+        )
+
+        updateMovie(
+            name: String
+            director_name: String!
+            production_house: String!
+            release_date: String!
+            rating: FlOAT!
+        )
+
+        deleteMovie(id: String!): Movie
+    }
+`;
+
+export default movieSchema;

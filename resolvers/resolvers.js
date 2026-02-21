a889ed4f34693bc57e88f16c8fdfc7ca8f7cc9c3
Harrison Pape <harrison.pape@georgebrown.ca>
2026-02-20 20:51:35 -0500

    Movie mutations created

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -65,5 +65,94 @@ const movieResolvers = {
             }
             return movies;
         }
+    },
+    Mutation: {
+        addMovie: async (_, args) => {
+            if (!args.name || !args.director_name || !args.production_house || args.release_date || args.rating) {
+                throw new GraphQLError("All parameters are required", {
+                    extensions: {
+                        code: "MISSING_PARAM",
+                        field: 'all',
+                        http: { status: 400 }
+                    }
+                });
+            }
+            let newMovie = new MovieModel({
+                name: args.name,
+                director_name: args.director_name,
+                production_house: args.production_house,
+                release_date: args.release_date,
+                rating: args.rating
+            })
+
+            const movie = await newMovie.save()
+
+            if (!movie) {
+                throw new GraphQLError("Movie not inserted", {
+                    extensions: {
+                        code: "MOVIE_NOT_INSERTED",
+                        field: 'new movie',
+                        http: { status: 400 }
+                    }
+                });
+            }
+            return movie;
+        },
+        updateMovie: async (_, args) => {
+            if (!args.id || !args.name || !args.director_name || !args.production_house || args.release_date || args.rating) {
+                throw new GraphQLError("All parameters are required", {
+                    extensions: {
+                        code: "MISSING_PARAM",
+                        field: 'all',
+                        http: { status: 400 }
+                    }
+                });
+            }
+            const movie = await MovieModel.findByIdAndUpdate(
+                args.id,
+                {
+                    name: args.name,
+                    director_name: args.director_name,
+                    production_house: args.production_house,
+                    release_date: args.release_date,
+                    rating: args.rating
+                },
+                { new: false }
+            )
+
+            if (!movie) {
+                throw new GraphQLError("Movie not updated", {
+                    extensions: {
+                        code: "MOVIE_NOT_UPDATED",
+                        field: 'id',
+                        http: { status: 400 }
+                    }
+                });
+            }
+            return movie
+        },
+        deleteMovie: async (_, args) => {
+            if (!args.id) {
+                throw new GraphQLError("Id parameter is required", {
+                    extensions: {
+                        code: "MISSING_PARAM",
+                        field: 'id',
+                        http: { status: 400 }
+                    }
+                });
+            }
+            const movie = await MovieModel.findByIdAndDelete(args.id)
+
+            if (!movie) {
+                throw new GraphQLError("Movie not deleted", {
+                    extensions: {
+                        code: "MOVIE_NOT_DELETED",
+                        field: 'id',
+                        http: { status: 400 }
+                    }
+                });
+            }
+            return movie;
+        }
     }
 }
